257fbbc228f2949be722ca9d29d0de37e0daad00
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-07 18:29:28 -0300

    :sparkles: newUser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,16 @@ const { User } = require('../models');
 
 const getByEmail = (email) => User.findOne({ where: { email } });
 
+const createUser = (displayName, email, password, image) => {
+    const newUser = User.create({
+        displayName,
+        email,
+        password,
+        image,
+    });
+    return newUser;
+};
 module.exports = {
     getByEmail,
+    createUser,
 };
