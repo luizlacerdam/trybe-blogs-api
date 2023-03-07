b6dc98868d2e7a47170b6ffd770ed88e05300c76
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-07 20:24:32 -0300

    :sparkles: createUser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,13 @@
 const { User } = require('../models');
+const schema = require('./validations/validationUser');
 
 const getByEmail = (email) => User.findOne({ where: { email } });
 
-const createUser = (displayName, email, password, image) => {
-    const newUser = User.create({
-        displayName,
-        email,
-        password,
-        image,
-    });
-    return newUser;
+const createUser = (newUser) => {
+    const error = schema.validateUser(newUser);
+    if (error.type) return error;
+    const userCreated = User.create(newUser);
+    return { type: null, message: userCreated };
 };
 module.exports = {
     getByEmail,
