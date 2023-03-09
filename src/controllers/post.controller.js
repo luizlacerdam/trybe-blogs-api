268b056a3361a3a560bea21721b63991a9fd6a57
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-09 18:08:23 -0300

    :sparkles: postController

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,29 @@
+const { PostService } = require('../services');
+
+const getAll = async (req, res) => {
+    try {
+        const posts = await PostService.getAll();
+        return res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { type, message } = await PostService.getById(id);
+        if (type) return res.status(404).json({ message });
+        
+        return res.status(200).json(message);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getAll,
+    getById,
+};
