8ed60539ab1980653b9c3c7fcd617f5476d384d5
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-10 11:07:20 -0300

    :sparkles: [feat] creatPost-router

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -5,5 +5,6 @@ const validateJWT = require('../auth/validateJWT');
 const router = express.Router();
 router.get('/:id', validateJWT, PostController.getById);
 router.get('/', validateJWT, PostController.getAll);
+router.post('/', validateJWT, PostController.createPost);
 
 module.exports = router;
